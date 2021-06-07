5d130343f5ffb3bb8fa5c9aa972362242586e8d3
khoacao0601 <khoacao0601@gmail.com>
2021-06-07 15:59:53 -0500

    Update

---
+++ components/CategoryList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import Paw from '../assets/paw.png';
+
+const Category = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Category ListItems</Text>
+            <Image style={styles.categoryImage} source={Paw}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    categoryImage: {
+        width: 180,
+        height: 180,
+        //left: 300
+    },
+    title: {
+        textTransform: 'uppercase',
+        marginBottom: 8,
+        fontWeight: '700',
+        fontSize: 75
+    },
+    container: {
+        alignItems: 'center',
+        padding: 16,
+        borderRadius: 4,
+        backgroundColor: '#FFF',
+        shadowColor: '#000',
+        shadowOpacity: 0.3,
+        shadowRadius: 50,
+        shadowOffset: { width: 0, height: 0},
+        marginBottom: 56
+    }
+});
+
+export default Category;
